1225fd7aa2e462780154fc24b8595896fb6810a6
katkaaa <98588418+katkaaa@users.noreply.github.com>
2023-05-11 18:03:19 +0200

    feature: verification attempts logging

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import lightbulb
 from dotenv import load_dotenv
 import json
 import os
+from datetime import datetime
 
 load_dotenv()
 
@@ -20,6 +21,8 @@ async def verifing(ctx : lightbulb.Context):
     data = json.load(f)
     success = hikari.Embed(title = "Ověření proběhlo úspěšně! <3", description = "Gratulujeme k přijetí na SSPŠ.\nByla ti přidělěna role, která ti odemkne zbytek serveru.", color = "#6888f2")
     fail = hikari.Embed(title = "Ověření neproběhlo úspěšně! </3", description = "Ajaj, něco je špatně. Zkus to znovu.\nPokud se tato chyba opakuje, napiš Katka#9714", color = "#6888f2")
+    logS = hikari.Embed(title = "Pokus o ověření", description = f"Pokus **byl** úspěšný\nPoužité evidenční číslo: `{ctx.options.number}`", timestamp = datetime.now().astimezone()).set_footer(text = f"{ctx.author}", icon = ctx.author.avatar_url)
+    logF = hikari.Embed(title = "Pokus o ověření", description = f"Pokus **nebyl** úspěšný\nPoužité evidenční číslo: `{ctx.options.number}`", timestamp = datetime.now().astimezone()).set_footer(text = f"{ctx.author}", icon = ctx.author.avatar_url)
     try: 
         if data[ctx.options.number] == True:
             filer = open("registry.json", "r")
@@ -29,10 +32,12 @@ async def verifing(ctx : lightbulb.Context):
                 data.pop(str(ctx.options.number))
                 json.dump(data, file)
                 
-            await ctx.member.add_role(1105490100114178078)
+            await ctx.member.add_role("insert role id")
             await ctx.respond(embed=success, flags = hikari.MessageFlag.EPHEMERAL)
+            await ctx.bot.rest.create_message("insert channel id", embed = logS)
     except KeyError:
         await ctx.respond(embed=fail, flags = hikari.MessageFlag.EPHEMERAL)
+        await ctx.bot.rest.create_message("insert channel id", embed = logF)
     finally:
         pass
 
